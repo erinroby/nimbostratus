a7028ec4ffc218f160e0983577365891e02fd88c
Erin Roby <erinroby@Erins-MacBook-Pro.local>
2016-12-06 08:14:33 -0800

    boilerplate

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+import HomePage from './components/HomePage/HomePage.js'
+
+if (process.env.NODE_ENV !== 'production') {
+  React.Perf = require('react-addons-perf')
+}
+
+render(<HomePage />, document.getElementById('app'))
